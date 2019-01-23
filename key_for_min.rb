0aafbccee253765eb369d4c30ece620a537529e4
David Gilbert <dagilbert333@gmail.com>
2019-01-23 00:11:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_value = nil
   if name_hash != {}
     name_hash.each do |key, value|
-      
+      if 
     end
   elsif name_hash == {}
     return min_value
