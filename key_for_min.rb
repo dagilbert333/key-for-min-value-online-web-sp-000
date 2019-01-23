bd94e6d9781739fbd20c281ce0cf3eb2b28cd557
David Gilbert <dagilbert333@gmail.com>
2019-01-23 00:47:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,15 +4,11 @@
 def key_for_min_value(name_hash)
   min_key = nil
   min_value = nil
-  if name_hash != {}
-    name_hash.each do |key, value|
-      if value < min_value
+  name_hash.each do |key, value|
+    if min_value == nil || value < min_value
         min_value = value
         min_key = key
       end
     end
-  elsif name_hash == {}
-    return min_key
-  end
   min_key
 end
